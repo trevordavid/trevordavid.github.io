cb5db8f939fca28f30e348a63837c9e80e2a81a3
Trevor David <trevorjdavid@gmail.com>
2026-02-18 14:07:12 -0500

    Add lightweight CI/CD workflow for GitHub Pages

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    "eslint:recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "script"
+  },
+  rules: {
+    "no-console": "off"
+  }
+};
